298b29a52693a972d88054babd93356cb42c8f5e
Java-Programmer-CF <107807895+Java-Programmer-CF@users.noreply.github.com>
2023-05-01 15:17:24 +0530

    Add files via upload

---
+++ untitled1/src/test/java/AppTest.java
@@ -0,0 +1,11 @@
+import org.junit.*;
+
+import static org.junit.Assert.assertTrue;
+
+
+public class AppTest {
+    @Test
+    public void shouldAssertWithTrue(){
+        assertTrue (true);
+    }
+}
